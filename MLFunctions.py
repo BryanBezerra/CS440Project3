4395fa094b3efc3740e4ddd74cdfe3fa7c7e4a0c
selimerhan <seliwerhau@gmail.com>
2023-12-11 17:13:52 -0500

    second task created

--- MLFunctions.py
+++ MLFunctions.py
@@ -1,92 +1,97 @@
+from CS440Project3.BombDiagram import BombDiagram
 import numpy as np
 
-
-# it turns the result of dot product into a probability thats between 0 to 1.
-# activation function (non-linear function)
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
-
-
-# f(x_1,x_2,...,x_d) = sigmoid(w_0 + x_1 w_1 + x_2 w_2 + ... + w_d x_d)
-def predict(inputVector, weightVector):
-    result = np.dot(inputVector, weightVector)
-    return sigmoid(result)
+class firstTask:
+    # it turns the result of dot product into a probability thats between 0 to 1.
+    # activation function (non-linear function)
+    
+    def __init__(self):
+        self.numFeatures = len(BombDiagram(20).get_flat_image)
+    
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
 
 
-# loss = -y_i * log(f(x_i)) - (1-y_i) * log(1 - f(x_i))
-def binary_cross_entropy_loss(yTrue, yPredicted):
-    # small constant to avoid log(0)
-    epsilon = 1e-15
-    loss = -yTrue * np.log(
-        np.clip(yPredicted, epsilon, 1 - epsilon) - (1 - yTrue) * np.log(np.clip(1 - yPredicted, epsilon, 1 - epsilon)))
-    return loss
+    # f(x_1,x_2,...,x_d) = sigmoid(w_0 + x_1 w_1 + x_2 w_2 + ... + w_d x_d)
+    def predict(self, inputVector, weightVector):
+        result = np.dot(inputVector, weightVector)
+        return self.sigmoid(result)
 
 
-# not too sure about this but it should represent the gradient as below
-# −y_i ​log(f(x_i​))−(1−y_i​)log(1−f(x_i​))
-def gradient_binary_cross_entropy(yTrue, yPredicted, x_j):
-    gradient = -yTrue * (1 - yPredicted) * x_j + (1 - yTrue) * yPredicted * x_j
-    return gradient
+    # loss = -y_i * log(f(x_i)) - (1-y_i) * log(1 - f(x_i))
+    def binary_cross_entropy_loss(self, yTrue, yPredicted):
+        # small constant to avoid log(0)
+        epsilon = 1e-15
+        loss = -yTrue * np.log(
+            np.clip(yPredicted, epsilon, 1 - epsilon) - (1 - yTrue) * np.log(np.clip(1 - yPredicted, epsilon, 1 - epsilon)))
+        return loss
 
 
-def linear_regression_loss(y_dang_or_not, f_x, weight):
-    epsilon = 1e-15
-    # loss_fun = np.sum(((-1) * y_dang_or_not * np.log(model_space(f_x, weight)+epsilon) - (1 - y_dang_or_not) * (1 - model_space(f_x, weight)+epsilon)))
-    loss_fun = np.mean(np.sum(np.power((np.dot(f_x, weight) - y_dang_or_not), 2)))  # linear regression loss
-    return loss_fun
+    # not too sure about this but it should represent the gradient as below
+    # −y_i ​log(f(x_i​))−(1−y_i​)log(1−f(x_i​))
+    def gradient_binary_cross_entropy(self, yTrue, yPredicted, x_j):
+        gradient = -yTrue * (1 - yPredicted) * x_j + (1 - yTrue) * yPredicted * x_j
+        return gradient
 
 
-weights = np.random.randn(numFeatures, 1)
-
-def train_linear_regression_sgd(x, y, learningRate, numEpochs, starting_weights):
-    numExamples, numFeatures = x.shape
+    def linear_regression_loss(self, y_dang_or_not, f_x, weight):
+        epsilon = 1e-15
+        # loss_fun = np.sum(((-1) * y_dang_or_not * np.log(model_space(f_x, weight)+epsilon) - (1 - y_dang_or_not) * (1 - model_space(f_x, weight)+epsilon)))
+        loss_fun = np.mean(np.sum(np.power((np.dot(f_x, weight) - y_dang_or_not), 2)))  # linear regression loss
+        return loss_fun
+    
     # TODO make weight range from [-0.025, 0.025]
+    def weight_initialization(self, numFeatures):
+        return np.random.randn(numFeatures, 1)
 
-    for epoch in range(numEpochs):
+    def train_linear_regression_sgd(self, x, y, learningRate, numEpochs, starting_weights):
+        numExamples = x.shape
 
-        for i in range(numExamples):
-            # random data point
-            random_index = np.random.randint(numExamples)
-            observed_in = x[random_index:random_index + 1, :]
-            observed_out = y[random_index:random_index + 1]
+        for epoch in range(numEpochs):
 
-            # forward pass
-            prediction = sigmoid(predict(observed_in, weights))
+            for i in range(numExamples):
+                # random data point
+                random_index = np.random.randint(numExamples)
+                observed_in = x[random_index:random_index + 1, :]
+                observed_out = y[random_index:random_index + 1]
 
-            loss = binary_cross_entropy_loss(observed_out, prediction)
+                # forward pass
+                prediction = self.sigmoid(self.predict(observed_in, weights))
 
-            gradient = gradient_binary_cross_entropy(observed_out, prediction, observed_in.T)
+                loss = self.binary_cross_entropy_loss(observed_out, prediction)
 
-            # update weights using stochastic gradient descent
-            weights -= learningRate * gradient
+                gradient = self.gradient_binary_cross_entropy(observed_out, prediction, observed_in.T)
 
-    return weights, loss
+                # update weights using stochastic gradient descent
+                weights -= learningRate * gradient
 
-def stochastic_gradient_descent(samples, diagram_size, num_epochs, alpha, bomb_diagram):
-    (num_image,) = np.shape(samples)
-    # index = np.random.randint(num_image)
+        return weights, loss
 
-    #TODO make weight range from [-0.025, 0.025]
-    weight = np.random.randn(diagram_size * diagram_size * 4 + 1, 1) * 0.001
-    print(weight)
+    def stochastic_gradient_descent(self, samples, diagram_size, num_epochs, alpha, bomb_diagram):
+        (num_image,) = np.shape(samples)
+        # index = np.random.randint(num_image)
 
-    for epoch in range(num_epochs):
-        for i in range(num_image):
-            random_x = np.random.randint(num_image)
-            input = samples[random_x]
-            observed_in = [BombDiagram.get_flat_image(input)]
-            observed_out = BombDiagram.is_dangerous(input)
-            if observed_out:
-                observed_out = 1
-            else:
-                observed_out = 0
-            # fun_x = model_space(input_x, weight)
-            z = (np.dot(observed_in, weight))
-            print("loss: ", linear_regression_loss(observed_out, observed_in, weight), end="\n")
+        #TODO make weight range from [-0.025, 0.025]
+        weight = np.random.randn(diagram_size * diagram_size * 4 + 1, 1) * 0.001
+        print(weight)
 
-            weight = sigmoid(z)  # SGD with linear regression
-            # weight = weight -
+        for epoch in range(num_epochs):
+            for i in range(num_image):
+                random_x = np.random.randint(num_image)
+                input = samples[random_x]
+                observed_in = [BombDiagram.get_flat_image(input)]
+                observed_out = BombDiagram.is_dangerous(input)
+                if observed_out:
+                    observed_out = 1
+                else:
+                    observed_out = 0
+                # fun_x = model_space(input_x, weight)
+                z = (np.dot(observed_in, weight))
+                print("loss: ", self.linear_regression_loss(observed_out, observed_in, weight), end="\n")
 
-            # print("\n", weight)
+                weight = self.sigmoid(z)  # SGD with linear regression
+                # weight = weight -
 
-    return weight
+                # print("\n", weight)
+
+        return weight
---
+++ SecondTask.py
@@ -0,0 +1,14 @@
+# Input Space 
+# Same as the first task which is vector size of 1601
+
+# Output Space
+# The color of the third laid down wire. Blue, Yellow, Green or Red 
+
+# Model Space
+# Multi Class Classification
+
+# Loss Function
+# Categorical cross entropy loss (softmax function)
+
+# Training Algorithm
+# 
